3f61629d500646fcd5dcba3e402b8af1f50e65b0
Vlad Smirnov <9075130@mail.ru>
2020-06-12 16:01:25 +0300

    ALG.lesson_9

--- algorithms/lesson_9/les_9_task_1.py
+++ algorithms/lesson_9/les_9_task_1.py
@@ -7,4 +7,14 @@
 или любой другой из модуля hashlib задача считается не решённой.
 '''
 
-import hashlib
+def count_substring(s: str) :
+    set_hash = set()
+    for i in range(len(s) - 1):
+        for j in range(i + 1, len(s) + 1):
+            set_hash.add(hash(s[i:j]))
+    counter = len(set_hash) - 1	# вычитаем совпадение строки с самой собой
+    return counter
+
+my_str = input('Введите строку для проверки: ')
+count = count_substring(my_str)
+print(f'B строке "{my_str}" есть {count} различных подстрок')
